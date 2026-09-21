bda68fb01b9a2c814f92b572f96a8bf1a6a61510
Divyansh Yadav <divyanshyd9@gmail.com>
2026-09-22 01:13:43 +0600

    Time: 7 ms (39.55%), Space: 17.8 MB (13.39%) - LeetHub

--- 0506-relative-ranks/0506-relative-ranks.cpp
+++ 0506-relative-ranks/0506-relative-ranks.cpp
@@ -1,52 +1,31 @@
 class Solution {
 public:
     vector<string> findRelativeRanks(vector<int>& score) {
-        
         int n = score.size();
 
-        // Bubble sort in descending order
-        // But store original index along with score
-        vector<pair<int, int>> arr;
+        vector<pair<int, int>> athletes;
 
         for (int i = 0; i < n; i++) {
-            arr.push_back({score[i], i});
+            athletes.push_back({score[i], i});
         }
 
-        for (int i = 0; i < n - 1; i++) {
-            int swap_count = 0;
+        sort(athletes.rbegin(), athletes.rend());
 
-            for (int j = 0; j < n - i - 1; j++) {
-                if (arr[j].first < arr[j + 1].first) {
-                    swap(arr[j], arr[j + 1]);
-                    swap_count++;
-                }
-            }
+        vector<string> ans(n);
 
-            if (swap_count == 0) {
-                break;
-            }
+        for (int rank = 0; rank < n; rank++) {
+            int index = athletes[rank].second;
+
+            if (rank == 0)
+                ans[index] = "Gold Medal";
+            else if (rank == 1)
+                ans[index] = "Silver Medal";
+            else if (rank == 2)
+                ans[index] = "Bronze Medal";
+            else
+                ans[index] = to_string(rank + 1);
         }
 
-        vector<string> answer(n);
-
-        for (int i = 0; i < n; i++) {
-
-            int originalIndex = arr[i].second;
-
-            if (i == 0) {
-                answer[originalIndex] = "Gold Medal";
-            }
-            else if (i == 1) {
-                answer[originalIndex] = "Silver Medal";
-            }
-            else if (i == 2) {
-                answer[originalIndex] = "Bronze Medal";
-            }
-            else {
-                answer[originalIndex] = to_string(i + 1);
-            }
-        }
-
-        return answer;
+        return ans;
     }
 };
